559c4acf90f343abdaa5067dc988d0c1dd8e6a29
Riaz Ahmad <riahmad@extendhealth.com>
2024-12-24 17:07:40 -0600

    abaya girl customizations

---
+++ src/Build/deploy.ps1
@@ -0,0 +1,16 @@
+﻿Push-Location -Path ..\Presentation\Nop.Web
+
+Write-Host "Clearing Published Files ..."
+Remove-Item -Path .\publish -Recurse -ErrorAction SilentlyContinue
+
+Write-Host "Building ..."
+dotnet restore
+dotnet publish -c Release -o ./publish
+
+
+Write-Host "Zipping ..."
+& {cmd /c '"C:\Program Files\7-Zip\7z.exe" a -tzip -o.\ abayagirl.zip .\publish\* -aoa'}
+
+az webapp deployment source config-zip --resource-group AbayaGirl --name abayagirl-app --src .\abayagirl.zip
+
+Pop-Location
